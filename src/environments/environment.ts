547d821b0ad953bccf7b80d36c4ba680afbbfcee
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-04-12 19:49:02 +0530

    Basic configuration done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDHD11FkO0VSwHTZQcFqrsN3IktqnsvN7E",
+    authDomain: "todowithfirebase-c8523.firebaseapp.com",
+    databaseURL: "https://todowithfirebase-c8523.firebaseio.com",
+    projectId: "todowithfirebase-c8523",
+    storageBucket: "todowithfirebase-c8523.appspot.com",
+    messagingSenderId: "717044810797",
+    appId: "1:717044810797:web:5cee137c6416c9c777a4a4",
+    measurementId: "G-8FJDJE5GL9"
+  }
 };
 
 /*
