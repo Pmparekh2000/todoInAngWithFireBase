29d77799d6e5909830df5a953ad98568960566dd
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-04-12 20:13:39 +0530

    Service made and injected

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from "./shared/todo.service";
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  providers: [TodoService]
 })
 export class TodoComponent implements OnInit {
-
-  constructor() { }
+  toDoListArray: any[];
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoService.getToDoList().snapshotChanges().subscribe(item=>{
+      this.toDoListArray = [];
+      item.forEach(element=>{
+        var x = element.payload.toJSON();
+        x["$key"] = element.key;
+        this.toDoListArray.push(x);
+      })
+      //sort array isChecker from false->true
+      this.toDoListArray.sort((a, b)=>{
+        return a.isChecked-b.isChecked;
+      })
+    });
+
+    
   }
 
 }
